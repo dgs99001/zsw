3f0f55e3fa36f801d453747905653921309690e6
dgs99001 <dgs99001@proton.me>
2026-05-27 22:03:17 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const apps = [
   
   { 
     id: 1, 
-    name: "戴高乐",
+    name: "戴高乐 ",
     description: "", 
     iconUrl: "https://pic1.imgdb.cn/item/6a11b0c96211f5ac19ec7cea.jpg",
     url: "",
